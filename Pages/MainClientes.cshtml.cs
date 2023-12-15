da2895f9c46b237459fa1fb4d713a0b1202b4811
VeoNoporSad <149404902+VeoNoporSad@users.noreply.github.com>
2023-12-14 21:23:34 -0400

    List Customers.

--- Pages/MainClientes.cshtml.cs
+++ Pages/MainClientes.cshtml.cs
@@ -1,11 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
-namespace TaxiToxic.ini.Pages;
-
-public class MainClientes : PageModel
+namespace TaxiToxic.ini.Pages
 {
-    public void OnGet()
+    public class MainClientes : PageModel
     {
-        
+        public List<Customer> Customers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync("https://localhost:7293/api/Customer");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        Customers = JsonConvert.DeserializeObject<List<Customer>>(apiResponse);
+                    }
+                    else
+                    {
+                        // Maneja el código de estado no exitoso
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Maneja la excepción
+                }
+            }
+        }
+    }
+
+
+    public class Customer
+    {
+        [Key]
+        public int Id_Customer { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string StartTimeState { get; set; } = null!;
+        public string EndTimeState { get; set; } = null!;
+        public DateTime LastAccess { get; set; }
     }
 }
